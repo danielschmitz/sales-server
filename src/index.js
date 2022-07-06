ee144624fb09e3e8afa82399afc5078cadf6f055
Daniel Schmitz <danieljfa@gmail.com>
2022-07-06 12:14:24 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const app = express()
 
 const libs = require('./libs')
 
-console.log("\x1b[33m%s\x1b[0m", `[Starting...]`, "\x1b[0m")
 console.log("\x1b[36m%s\x1b[0m", `[loading libs]`, "\x1b[0m", `${libs}`)
 libs.forEach(lib => {
     require(`./libs/${lib}`)(app)
