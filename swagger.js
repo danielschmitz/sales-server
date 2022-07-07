334b2f1189aed03f1347bb8936433014927f53c5
Daniel Schmitz <danieljfa@gmail.com>
2022-07-07 09:59:55 -0300

    With Autogen

---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const doc = {
+    info: {
+        title: 'Sales Server',
+        description: 'Documentation API',
+        version: '1.0',
+        contact: {
+            name: 'Daniel Schmitz',
+            email: 'danieljfa@gmail.com',
+            url: 'https://github.com/danielschmitz'
+        }
+    },
+    host: 'localhost:3000',
+    basePath: '/api'
+}
+
+const outputFile = './src/swagger.json'
+const endpointsFiles = ['./src/api/category.js']
+
+swaggerAutogen(outputFile, endpointsFiles, doc)
+
