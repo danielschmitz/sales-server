bba5d36e65d5886d94f40e3b3dba3e60becaa71e
Daniel Schmitz <danieljfa@gmail.com>
2022-07-07 09:45:23 -0300

    First version of swagger

---
+++ src/libs/swagger.js
@@ -0,0 +1,6 @@
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../swagger.json')
+
+module.exports = app => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+}
