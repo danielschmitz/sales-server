047e859641a0b7095fe75b282b28f155c800246b
Daniel Schmitz <danieljfa@gmail.com>
2022-07-08 10:30:23 -0300

    api-docs url

--- src/libs/start.js
+++ src/libs/start.js
@@ -2,6 +2,6 @@
 module.exports = app => {
     const port = process.env.PORT || 3000
     app.listen(port, () => {
-        console.log("\x1b[33m%s\x1b[0m", `[Started]`, "\x1b[0m", `http://localhost:${port}`)
+        console.log("\x1b[33m%s\x1b[0m", `[Started]`, "\x1b[0m", `Check docs at http://localhost:${port}/api-docs`)
     })
 }
