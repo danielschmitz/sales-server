8d6785009334a3100f223f3d0ad88fc4a24e4674
Daniel Schmitz <danieljfa@gmail.com>
2022-07-17 09:26:23 -0300

    handle non numeric errors

--- src/libs/error.js
+++ src/libs/error.js
@@ -1,5 +1,11 @@
 module.exports = app => {
   app.use(async (err, req, res, _next) => {
+    
+    //try to handle string errors TODO: verify is string 
+    if (err.code == 'SQLITE_ERROR') {
+      err.code = 500
+    }
+    
     const httpCode = err.code || 500
     res.status(httpCode).send({ message: err.message, code: httpCode, name: err.name })
   })
