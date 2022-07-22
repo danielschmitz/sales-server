c7d8d27061e5aa82159e12ae4951c6411677bc98
Daniel Schmitz <danieljfa@gmail.com>
2022-07-22 11:27:23 -0300

    ♻️ using prvate methods

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -4,18 +4,28 @@ const NotFoundError = require("../errors/NotFoundError")
 const Joi = require('joi')
 const table = require("../constants/table")
 
-const categorySchema = Joi.object({
-    name: Joi.string()
-        .min(3)
-        .max(30)
-        .required(),
-    description: Joi.string()
-        .max(30)
-})
-
-
 class CategoryService {
     constructor() {
+    }
+    async validate(data) {
+        const categorySchema = Joi.object({
+            name: Joi.string()
+                .min(3)
+                .max(30)
+                .required(),
+            description: Joi.string()
+                .max(30)
+        })
+        try {
+            await categorySchema.validateAsync(data)
+        } catch (error) {
+            throw new BadInputError(error.message)
+        }
+    }
+    async exists(id) {
+        const result = await db(table.categories).where({ id })
+        if (result.length == 0) throw new NotFoundError('Category not found')
+        return result
     }
     /**
      * Get all Categories
@@ -27,23 +37,16 @@ class CategoryService {
      * Get a Category by id
      */
     async findById(id) {
-
         if (!parseInt(id)) throw new BadInputError('Invalid id')
-
-        const result = await db(table.categories).where({ id })
-        if (result.length == 0) throw new NotFoundError('Category not found')
-
-        return result
+        return await this.exists(id)
     }
     /**
      * Create a new Category
      */
     async create(category) {
-        try {
-            await categorySchema.validateAsync(category)
-        } catch (error) {
-            throw new BadInputError(error.message)
-        }
+
+        await this.validate(category)
+
         const { name, description } = category
         const checkName = await db(table.categories).where({ name }).first()
         if (checkName !== undefined) throw new Error('There is already a category with that name')
@@ -59,11 +62,9 @@ class CategoryService {
     * Update a category
     */
     async update(id, category) {
-        try {
-            await categorySchema.validateAsync(category)
-        } catch (error) {
-            throw new BadInputError(error.message)
-        }
+
+        await this.validate(category)
+
         const { name, description } = category
         const checkName = await db(table.categories).where({ name }).whereNot('id', id).first()
 
@@ -79,8 +80,7 @@ class CategoryService {
     async delete(id) {
         if (!parseInt(id)) throw new BadInputError('Invalid id')
 
-        const result = await db(table.categories).where({ id })
-        if (result.length == 0) throw new NotFoundError('Category not found')
+        this.exists(id)
 
         await db(table.categories).where({ id }).delete()
 
