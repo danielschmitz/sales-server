4240463dbf3509d44e9854328d8d24bb5a3513a7
Daniel Schmitz <danieljfa@gmail.com>
2022-07-25 12:35:26 -0300

    🚀 heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,10 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     pool: {
       min: 2,
       max: 10
