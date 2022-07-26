840dcdf56c8759b39f44b43ac3d7ee9a489d1ce7
Daniel Schmitz <danieljfa@gmail.com>
2022-07-26 09:25:00 -0300

    ✨ redirect to swagger

--- src/libs/swagger.js
+++ src/libs/swagger.js
@@ -3,4 +3,7 @@ const swaggerDocument = require('../swagger.json')
 
 module.exports = app => {
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    app.get('/', function (_req, res) {
+        res.redirect('/api-docs')
+    })
 }
